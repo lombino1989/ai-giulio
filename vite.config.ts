47f003ab37643f27e8880fe087dbeea4c6bf922b
lombino1989 <128836961+lombino1989@users.noreply.github.com>
2025-05-24 16:22:55 +0200

    1

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/ai-giulio/', // <--- AGGIUNGI QUESTA LINEA (sostituisci 'ai-giulio' se necessario)
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
